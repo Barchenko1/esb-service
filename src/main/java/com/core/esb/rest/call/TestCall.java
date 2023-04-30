78733302348b1afafa92a0fe361489f80cb5f118
Pavel Barchenko <Pavel.Barchenko@ibm.com>
2023-04-30 13:35:29 +0200

    add properties

--- src/main/java/com/core/esb/rest/call/TestCall.java
+++ src/main/java/com/core/esb/rest/call/TestCall.java
@@ -2,18 +2,20 @@ package com.core.esb.rest.call;
 
 import com.core.esb.service.IServiceCall;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.core.esb.url.TdsApi.TDS_URL;
-
-
 @RestController
 @RequestMapping("/esb")
 public class TestCall {
+    @Value("${com.tds.url}")
+    private String tdsUrl;
+    @Value("${com.cs.url}")
+    private String cacheServiceUrl;
     private final IServiceCall<String> serviceCall;
 
     @Autowired
@@ -23,7 +25,7 @@ public class TestCall {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public ResponseEntity<String> getCall() {
-        String result = serviceCall.doCall(TDS_URL, String.class);
+        String result = serviceCall.doCall(cacheServiceUrl, String.class);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
--- src/main/java/com/core/esb/url/TdsApi.java
+++
@@ -1,5 +0,0 @@
-package com.core.esb.url;
-
-public interface TdsApi {
-    String TDS_URL = "http://localhost:8080/v1/view";
-}
